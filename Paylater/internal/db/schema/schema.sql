08bf878f1db07f413990f46dc608e9f43586a8e0
22K61A05A9 <haripriyanka.mutyala@sasi.ac.in>
2026-07-27 12:25:15 +0000

    27-07-2026

---
+++ Paylater/internal/db/schema/schema.sql
@@ -0,0 +1,25 @@
+create table users(
+    id int AUTO_INCREMENT primary key,
+    user_name varchar(100) not null,
+    email varchar(100) unique not null,
+    credit_limit decimal(10,2) not null,
+    repay decimal(10,2) default 0
+);
+create table merchants (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    merchant_name VARCHAR(100) NOT NULL,
+    phone_number VARCHAR(15),
+    commission_percentage DECIMAL(5,2) NOT NULL
+);
+create table transactions(
+    id int AUTO_INCREMENT PRIMARY KEY,
+    user_id int NOT NULL,
+    merchant_id INT NOT NULL,
+    amout DECIMAL(10,2) NOT NULL,
+    commission DECIMAL(10,2) NOT NULL,
+    commission_percentage DECIMAL(5,2) NOT NULL,
+    transaction_date TIMESTAMP default current_Timestamp,
+    transaction_type Enum('purchase','payback') Not null,
+    foreign key(user_id) references users(id),
+    foreign key(merchant_id) references merchants(id)
+);
